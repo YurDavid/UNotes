90db00cbe3b0542072b09e83b19f3c4b7c425389
YurDavid <davidyevu18@gmail.com>
2021-03-30 18:50:12 +0100

    set the endpoint for the put request

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import notes_list
+from .views import notes_list, notes_detail
 
 
-urlpatterns = [path("notes/", notes_list, name="notes_list")]
+urlpatterns = [path("notes/", notes_list, name="notes_list"),
+               path("notes/<int:pk>/", notes_detail, name="notes_detail")]
+
